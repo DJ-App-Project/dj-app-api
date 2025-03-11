4198b2b11b4caf9531d344787c1187e5edce1d09
ziga <zigastr1@gmail.com>
2025-03-11 20:18:19 +0100

    Setup + connection

---
+++ dj-api/Models/Song.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace dj_api.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Song
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("title")]
+        public string Title { get; set; }
+
+        [BsonElement("artist")]
+        public string Artist { get; set; }
+
+        [BsonElement("genre")]
+        public string Genre { get; set; }
+
+        [BsonElement("addedAt")]
+        public DateTime AddedAt { get; set; }
+    }
+}
